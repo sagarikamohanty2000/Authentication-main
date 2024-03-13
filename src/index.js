b45bc43cdf2ab2ae1845a3e5b6662c223d6232b4
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2024-03-13 21:44:35 +0530

    login_out_contextApi

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import{ AuthProvider} from './components/Store/AuthContext';
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <AuthProvider>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </AuthProvider>
 );
